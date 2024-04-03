50b0c1541d16eecff190b1890dfa81cddca0c394
Đoàn Nam <namgoku235@gmail.com>
2024-04-04 02:05:26 +0700

    add sentEmail

---
+++ sentEmail/sentEmail/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace sentEmail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendEmail = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txbInputEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendEmail
+            // 
+            this.btnSendEmail.AutoSize = true;
+            this.btnSendEmail.Location = new System.Drawing.Point(162, 155);
+            this.btnSendEmail.Name = "btnSendEmail";
+            this.btnSendEmail.Size = new System.Drawing.Size(81, 26);
+            this.btnSendEmail.TabIndex = 0;
+            this.btnSendEmail.Text = "sendEmail";
+            this.btnSendEmail.UseVisualStyleBackColor = true;
+            this.btnSendEmail.Click += new System.EventHandler(this.btnSendEmail_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(363, 120);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txbInputEmail
+            // 
+            this.txbInputEmail.Location = new System.Drawing.Point(350, 214);
+            this.txbInputEmail.Name = "txbInputEmail";
+            this.txbInputEmail.Size = new System.Drawing.Size(100, 22);
+            this.txbInputEmail.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbInputEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnSendEmail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txbInputEmail;
+    }
+}
+
---
+++ sentEmail/sentEmail/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sentEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendEmail(string recipientEmail, string subject, string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("namgoku235@gmail.com");
+                mail.To.Add(recipientEmail);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                SmtpServer.Port =587;
+                SmtpServer.Credentials = new NetworkCredential("namgoku235@gmail.com", "01668462570");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+
+                MessageBox.Show("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending email: " + ex.Message);
+            }
+        }
+        private string GenerateVerificationCode()
+        {
+            // Tạo mã xác nhận ngẫu nhiên (ví dụ: một chuỗi số có độ dài 6)
+            Random random = new Random();
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, 6)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int otp;
+        Random random = new Random();
+        private void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            #region 
+            /*
+            // Lấy email của người dùng từ TextBox
+            string recipientEmail = "doantuannam2206.sp@gmail.com";
+
+            // Tạo mã xác nhận (có thể sử dụng thuật toán tạo mã xác nhận ngẫu nhiên)
+            string verificationCode = GenerateVerificationCode();
+
+            // Gửi email chứa mã xác nhận
+            SendEmail(recipientEmail, "Verification Code", "Your verification code is: " + verificationCode);
+            */
+            #endregion
+
+            try
+            {
+                otp = random.Next(100, 1000);
+
+                var fromAddress = new MailAddress("namgoku235@gmail.com");// mail dùng để gửi mã otp 
+                var toAddress = new MailAddress("doantuannam2206.sp@gmail.com"); // mail dùng để nhận mã otp 
+
+
+                const string frompass = "putm xvul wdxi cdpp";
+                const string subject = "OTP code";
+                string body = otp.ToString();
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, frompass),
+                    Timeout = 200000
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body,
+                })
+                {
+                    smtp.Send(message);
+                }
+                MessageBox.Show("Otp đã được gửi qua mail");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+           
+        }
+    }
+}
